7e164bb73c1fd0092d16fadf60f8fcb0b206f5b5
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 06:44:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday << supply
+      holiday.each do |items|
+        items << supply
+      end
     end
   end
   
