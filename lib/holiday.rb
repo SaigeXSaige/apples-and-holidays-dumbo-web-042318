8b0c97b3269073c7736aace8e9cc7a2e501d737c
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 07:23:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday| 
-    print `#{season}: #{season.key}: #{season.values)
+    
     
     
   end
